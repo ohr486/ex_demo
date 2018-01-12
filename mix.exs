9504c7022f71df2bf8e081ff7d81bf4a8fbc2723
ohr486 <ohr486@gmail.com>
2018-01-12 17:27:28 +0900

    add deploy

--- mix.exs
+++ mix.exs
@@ -37,7 +37,11 @@ defmodule ExDemo.Mixfile do
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
       {:gettext, "~> 0.11"},
-      {:cowboy, "~> 1.0"}
+      {:cowboy, "~> 1.0"},
+      # for deployment
+      {:edeliver, "~> 1.4.4"},
+      {:distillery, "~> 1.4", runtime: false},
+      {:jsx, "~> 2.8"}
     ]
   end
 end
